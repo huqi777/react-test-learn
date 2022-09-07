10b1a9122b6ad968363165abe59821ddae3fa198
胡奇 <huqi_9607@qq.com>
2022-09-07 14:51:22 +0800

    push text

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Index from './pages/index'
 
 function App() {
+  const handle = (name: String) => {
+    console.log('zuihaos ', name)
+    setTimeout(() => {
+      Promise.resolve('name')
+    }, 5000);
+  }
+  // handle('123').then((res: any) => {
+  //   console.log(res);
+  // })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Index></Index>
     </div>
   );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+function index() {
+  return (
+    <div className='index'>
+      <div className='login'>
+        <div className="btn">登录</div>
+        <div className="back"></div>
+      </div>
+      <div className="content"></div>
+
+    </div>
+  )
+}
+
+export default index
